cb3a5ecabfe502df21418081ce02d60577511125
pusha31501 <pusha31501@gmail.com>
2025-11-04 20:17:58 +0800

    modify app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Index from "./pages";
-import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FlashCard from "./components/FlashCard";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-blue-700/80">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <FlashCard />
     </div>
   );
 };
